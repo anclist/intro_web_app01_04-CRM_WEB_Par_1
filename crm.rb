5cfe2cbbe6ceadf37c65d95b6c65bc689689f0fc
ANTONIO <ANCLIST@GMAIL.COM>
2017-10-03 22:18:43 -0400

    Display all contacts

--- crm.rb
+++ crm.rb
@@ -4,3 +4,15 @@ require 'sinatra'
 get '/' do
   erb :index
 end
+
+get '/contacts' do
+#In this new route, create an instance variable containing a collection
+#of all the contacts in your database
+  # @contacts = []
+  @contacts = Contact.all
+  erb :contacts
+end
+
+after do
+  ActiveRecord::Base.connection.close
+end
